4dd00d728dc99596405c9b67480a2e656fc7392a
Sebastian Galeano <sebascarp9@gmail.com>
2024-07-09 10:06:24 -0300

    footer added and imgs config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header.jsx';
 import Info from './components/Info.jsx';
 import './styles/main.css';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <Info />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer () {
+    return (
+        <>
+            <footer>
+                <p>&copy; <span>2024</span> The Red Coffe. Todos los derechos reservados.</p>
+            </footer>
+        </>
+    )
+
+}
