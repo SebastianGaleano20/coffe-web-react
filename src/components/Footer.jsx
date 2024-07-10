df97c525dad2815e4f92217dcdf68013926ce6f2
Sebastian Galeano <sebascarp9@gmail.com>
2024-07-10 10:52:32 -0300

    styles footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,21 @@
 import styles from '../styles/styles.module.css'
 import '../styles/main.css'
-import { FaInstagram, LiaFacebookSquare, FaPhone, MdOutlineMarkEmailRead, TbMapSearch   } from "react-icons/fa6";
+import { FaInstagram, FaFacebookF, FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineMarkEmailRead, MdOutlineMapsHomeWork } from "react-icons/md";
+
 
 export default function Footer() {
     return (
         <>
             <footer>
-                <div className="logo-container">
-                    <img
-                        className={styles.logo}
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ80A1T7fyKwl7SuoXkL1oVZaC9j37jh1bPhnQMVVHQjBC1p5ak2U2ddoR9Ba9sfF2Ickc&usqp=CAU"
-                        alt="logo"
-                    />
-                </div>
                 <div className={styles.contact}>
                     <h3>Contacto</h3>
                     <ul>
-                        <li><FaInstagram />Instagram</li>
-                        <li><LiaFacebookSquare />Facebook</li>
-                        <li><FaPhone />Telefono</li>
-                        <li><MdOutlineMarkEmailRead />Email</li>
-                        <li><TbMapSearch />Direccion</li>
+                        <li><a href="#"><FaInstagram />Instagram</a></li>
+                        <li><a href="#"><FaFacebookF />Facebook</a></li>
+                        <li><a href="#"><FaPhoneAlt />Telefono</a></li>
+                        <li><a href="#"><MdOutlineMarkEmailRead />Email</a></li>
+                        <li><a href="#"><MdOutlineMapsHomeWork />Direccion</a></li>
                     </ul>
                 </div>
                 <p>&copy; <span>2024</span> The Red Coffe. Todos los derechos reservados.</p>
